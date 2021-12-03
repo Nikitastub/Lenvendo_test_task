5b4e6e7969f9e2ee2de2ba17614ac236bb398375
Nikita St <nikita.stub@mail.ru>
2021-12-03 11:29:45 +0300

    minor changes

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,12 @@
 
 def test_api(api):
-    check_brand_list = []
     price_list = []
     response, sub_string = api.get_js_test_task()
     products = response['products']
     for product in products:
+        # checking "name" include sub_string
         assert sub_string in product['name']
         price_list.append(product['price'])
     # checking prices go from min to max
     assert price_list == sorted(price_list)
-    # checking "name" include sub_string
-    assert check_brand_list == []
 
