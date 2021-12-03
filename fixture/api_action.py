222e9492faec8f4c2974514bab293dbc17a976d6
Nikita St <nikita.stub@mail.ru>
2021-12-03 12:27:49 +0300

    minor changes (3)

--- fixture/api_action.py
+++ fixture/api_action.py
@@ -5,11 +5,15 @@ class ApiActionHelper:
     def __init__(self, base_url):
         self.base_url = base_url
 
-    def get_js_test_task(self, payload):
-        # payload = {'search': 'Huawei', 'sort_field': 'price'}
+    def get_js_test_task(self, payload=None):
         url = self.base_url + 'js-test-task/'
-        response = ((requests.get(url=url, params=payload)).json())
-        sub_string = payload['search']
-        return response, sub_string
+        req = requests.get(url=url, params=payload)
+        assert '200' in str(req)
+        res = req.json()
+        if payload is not None:
+            sub_string = payload['search']
+            return res, sub_string
+        else:
+            return res, None
 
 
