b34285af26c5afd44c971b1514c402b3aed9a45b
Nikita St <nikita.stub@mail.ru>
2021-12-03 12:42:19 +0300

    minor changes (4)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,14 @@
 
 def test_api(api):
     price_list = []
-    response, sub_string = api.get_js_test_task()
+    response, sub_string = api.get_js_test_task({'search':'Huawei', 'sort_field':'price'})
     products = response['products']
-    if sub_string is not None:
+    try:
         for product in products:
             # checking "name" include sub_string
             assert sub_string in product['name']
             price_list.append(product['price'])
         # checking prices go from min to max
         assert price_list == sorted(price_list)
-
+    except:
+        raise ValueError('запрос не содержит параметров')
